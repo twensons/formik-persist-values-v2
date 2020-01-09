7c9d7102d20ed06b42f85c3c032863dc20b08e08
Grigoriy Kolenko <kolenko@me.com>
2020-01-09 15:29:25 +0100

    Update rollup build config

--- rollup.config.js
+++ rollup.config.js
@@ -3,48 +3,74 @@ import filesize from 'rollup-plugin-filesize';
 
 import resolve from 'rollup-plugin-node-resolve';
 import sourceMaps from 'rollup-plugin-sourcemaps';
-import uglify from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
+
+const globalPackages = {
+  react: 'React',
+  formik: 'formik',
+  'prop-types': 'PropTypes',
+};
 
 const shared = {
-  entry: `compiled/PersistFormikValues.js`,
-  sourceMap: true,
-  external: ['react', 'formik', 'prop-types', 'lodash.isequal'],
-  globals: {
-    react: 'React',
-    formik: 'formik',
-    'prop-types': 'PropTypes',
-    'lodash.isequal': 'lodash.isequal',
-  },
-  exports: 'named',
+  input: `compiled/PersistFormikValues.js`,
+  plugins: [sourceMaps()],
+  external: Object.keys(globalPackages),
 };
 
 export default [
   Object.assign({}, shared, {
-    moduleName: 'FormikPersist',
+    moduleName: 'PersistFormikValues',
     format: 'umd',
-    dest: 'dist/PersistFormikValues.umd.js',
+    output: {
+      exports: 'named',
+      sourcemap: true,
+      global: globalPackages,
+      file: 'dist/PersistFormikValues.umd.js',
+    },
     plugins: [
       resolve(),
       commonjs({
         include: /node_modules/,
       }),
       sourceMaps(),
+      terser(),
       filesize(),
-      uglify(),
     ],
   }),
 
   Object.assign({}, shared, {
-    targets: [
-      { dest: 'dist/PersistFormikValues.es6.js', format: 'es' },
-      { dest: 'dist/PersistFormikValues.js', format: 'cjs' },
-    ],
+    output: {
+      exports: 'named',
+      sourcemap: true,
+      global: globalPackages,
+      file: 'dist/PersistFormikValues.es6.js',
+      format: 'es',
+    },
     plugins: [
       resolve(),
       commonjs({
         include: /node_modules/,
       }),
       sourceMaps(),
+      terser(),
+    ],
+  }),
+
+  Object.assign({}, shared, {
+    output: {
+      exports: 'named',
+      sourcemap: true,
+      global: globalPackages,
+      file: 'dist/PersistFormikValues.js',
+      format: 'cjs',
+    },
+    plugins: [
+      resolve(),
+      commonjs({
+        include: /node_modules/,
+      }),
+      sourceMaps(),
+      terser(),
     ],
   }),
 ];
